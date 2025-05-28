a8d311bef009290919c1a03a0675e8ab466ed74a
probin-dhakal <154975813+probin-dhakal@users.noreply.github.com>
2025-05-28 13:14:22 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,7 +17,7 @@ export default function App() {
     minutes: 0,
     seconds: 0
   });
-//final done
+
   useEffect(() => {
     if (selectedProblem) {
       document.body.style.overflow = 'hidden';
@@ -239,15 +239,6 @@ export default function App() {
         </div>
       </section>
 
-      
-<div style="text-align: center; margin-top: 20px;">
-  <a href="https://forms.gle/cpJdoQpAtenTg8nL9" target="_blank">
-    <button style="padding: 10px 20px; font-size: 16px; background-color: #007bff; color: white; border: none; border-radius: 5px; cursor: pointer;">
-      Register Now
-    </button>
-  </a>
-</div>
-        
       {/* Problem Statements */}
       <section id="problems" className="relative py-32 px-6 lg:px-8">
         <div className="max-w-7xl mx-auto">
